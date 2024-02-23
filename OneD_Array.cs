9cf2f31bb83a9041d13a39e46b117ebf8e5ba803
ASUS <ASUS@LAPTOP-4HFJC91M>
2024-02-23 17:13:05 +0300

    Добавьте файлы проекта.

---
+++ OneD_Array.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Задание_3_2.Interfaces;
+
+public sealed class OneD_Array : BaseArray, IOneD_Array
+{
+    private int[] _array;
+
+    public OneD_Array(int length, bool Keyboard_Input = false)
+    {
+        _array = new int[length];
+        Create(Keyboard_Input);
+    }
+
+    protected override void Initialize()
+    {
+        Random rnd = new Random();
+        for (int i = 0; i < _array.Length; i++)
+        {
+            _array[i] = rnd.Next(0, 150);
+        }
+    }
+
+    protected override void Keyboard_Initialize()
+    {
+        for (int i = 0; i < _array.Length; i++)
+        {
+            _array[i] = int.Parse(Console.ReadLine());
+        }
+    }
+
+    public override void Print()
+    {
+        Print_Array(_array);
+    }
+
+    private static void Print_Array(int[] _arr)
+    {
+        for (int i = 0; i < _arr.Length; i++)
+        {
+            Console.Write(_arr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public override double Mid_Value()
+    {
+        int value_sum = 0;
+        for (int i = 0; i < _array.Length; i++)
+        {
+            value_sum += _array[i];
+        }
+        return value_sum / _array.Length;
+    }
+
+    public void Delite_Values_More_Than_Mod_100()
+    {
+        int new_array_length = 0;
+
+        for (int i = 0; i < _array.Length; i++)
+        {
+            if (_array[i] > -100 && _array[i] < 100)
+            {
+                new_array_length++;
+            }
+        }
+
+        int[] new_array = new int[new_array_length];
+        int new_array_ind = 0;
+
+        for (int i = 0; i < _array.Length; i++)
+        {
+            if (_array[i] > -100 && _array[i] < 100)
+            {
+                new_array[new_array_ind] = _array[i];
+                new_array_ind++;
+            }
+        }
+        _array = new_array;
+    }
+
+    public void Delite_Unique_Values()
+    {
+        int new_arr_length = _array.Length;
+        for (int i = 0; i < _array.Length; i++)
+        {
+            for (int j = i; j < _array.Length; j++)
+            {
+
+                if (_array[i] == _array[j] && i != j)
+                {
+                    new_arr_length--;
+                    break;
+                }
+            }
+        }
+        int Counter = 0;
+        int[] new_arr = new int[new_arr_length];
+        for (int k = 0; k < _array.Length; k++)
+        {
+            if (!Contain(new_arr, _array[k]))
+            {
+                new_arr[Counter] = _array[k];
+                Counter++;
+            }
+        }
+        Print_Array(new_arr);
+    }
+
+    private static bool Contain(int[] arr, int symb)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (symb == arr[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Globalization;
+using Задание_3_2.Interfaces;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        BaseArray[] Base_Array = new BaseArray[3];
+
+        //Cоздание одномерного массива
+        Console.WriteLine("Введите длинну Вашего одномерного массива: ");
+        int Arr_Len = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Если вы хотите ввести значения с помощью клавиатуры введите true");
+        Console.WriteLine("Если вы не хотите вводить значения с помощью клавиатуры введите false");
+        bool _1DKeyboard_Input = bool.Parse(Console.ReadLine());  
+
+        Base_Array[0] = new OneD_Array(Arr_Len, _1DKeyboard_Input);
+
+        //Cоздание двумерного массива
+        Console.WriteLine("Введите количество строк в вашем двумерном массиве:");
+        int _2D_Arr_height = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество столбцов в вашем двумерном массиве:");
+        int _2D_Arr_length = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Если вы хотите ввести значения с помощью клавиатуры введите true");
+        Console.WriteLine("Если вы не хотите вводить значения с помощью клавиатуры введите false");
+        bool _2DKeyboard_Input = bool.Parse(Console.ReadLine());
+
+        Base_Array[1] = new TwoD_Array(_2D_Arr_length, _2D_Arr_height, _2DKeyboard_Input);
+
+        //Создание ступенчатого массива
+        Console.WriteLine("Введите количество подмассивов в вашем ступенчатом массиве:");
+        int Step_Arr_length = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Если вы хотите ввести значения с помощью клавиатуры введите true");
+        Console.WriteLine("Если вы не хотите вводить значения с помощью клавиатуры введите false");
+        bool _SDKeyboard_Input = bool.Parse(Console.ReadLine());
+
+        Base_Array[2] = new Step_Array(Step_Arr_length, _SDKeyboard_Input);
+
+        //Вывод всего массива и его среднего значения
+        for (int i = 0; i < Base_Array.Length; i++)
+        {
+            Console.WriteLine("Вывод массива:");
+            Base_Array[i].Print();
+            Console.WriteLine($"Среднее значение {Base_Array[i].Mid_Value()} ");
+        }
+
+        //Cоздание дня недели
+        Задание_3_2.Calendar cal = new Задание_3_2.Calendar();
+
+        //Создание массива всех ранее созданых объектов
+        IPrint[] new_arr = new IPrint[4];
+        new_arr[0] = Base_Array[0];
+        new_arr[1] = Base_Array[1];
+        new_arr[2] = Base_Array[2];
+        new_arr[3] = cal;
+
+        //Печать всех элементов из new_arr
+        Console.WriteLine("-----------------------------------");
+        for (int i = 0; i < new_arr.Length; i++)
+        {
+            Console.WriteLine("Печать элемента");
+            new_arr[i].Print();
+        }
+
+        //Пересоздание внутреннего массива в каждом экземпляре класса
+        //Вывод самого массива и его среднего
+        Console.WriteLine("-----------------------------------");
+        for (int i = 0; i < Base_Array.Length; i++)
+        {
+            Console.WriteLine("Вы хотите пересоздать массив с клавиатуры или автоматически?");
+            Console.WriteLine("Если вы хотите ввести значения с помощью клавиатуры введите true");
+            Console.WriteLine("Если вы не хотите вводить значения с помощью клавиатуры введите false");
+            bool flag = bool.Parse(Console.ReadLine());
+            Base_Array[i].Create(flag);
+
+            Console.WriteLine("Вывод пересозданного массива: ");
+            Base_Array[i].Print();
+
+            Console.WriteLine("Вывод среднего в пересозданном массиве: ");
+            Console.WriteLine(Base_Array[i].Mid_Value());
+        }
+
+        Console.ReadLine();
+    }
+}
